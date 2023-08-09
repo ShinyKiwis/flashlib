be4f29bd8028a02a9949f290d8d076c358b18881
Nguyen Hua <nguyenhua.hcm@gmail.com>
2023-08-09 16:55:56 +0700

    Update+Fixed: Implement switching questions and fix ui

--- app/javascript/controllers/study_controller.js
+++ app/javascript/controllers/study_controller.js
@@ -4,14 +4,32 @@ export default class extends Controller {
   static targets = ['question', 'answer', 'state', 'current']
   connect() {
     this.currentQuestion = 0
+    this.size = +this.data.get('size')
+    console.log(this.size)
     this.questionTarget.classList.remove("hidden")
   }
 
   reveal() {
-    console.log("MEEP")
-    console.log(this.questionTargets[this.currentQuestion])
     this.stateTarget.textContent = 'A'
     this.questionTargets[this.currentQuestion].classList.add("hidden")
     this.answerTargets[this.currentQuestion].classList.remove("hidden")
+  }
+
+  next() {
+    this.questionTargets[this.currentQuestion].classList.add("hidden")
+    this.answerTargets[this.currentQuestion].classList.add("hidden")
+    this.currentTarget.textContent = Math.min(this.size, +this.currentTarget.textContent + 1)
+    this.currentQuestion = Math.min(this.size-1, this.currentQuestion+1)
+    this.questionTargets[this.currentQuestion].classList.remove("hidden")
+    this.stateTarget.textContent = 'Q'
+  }
+
+  previous() {
+    this.questionTargets[this.currentQuestion].classList.add("hidden")
+    this.answerTargets[this.currentQuestion].classList.add("hidden")
+    this.currentQuestion = Math.max(0, this.currentQuestion-1)
+    this.questionTargets[this.currentQuestion].classList.remove("hidden")
+    this.stateTarget.textContent = 'Q'
+    this.currentTarget.textContent = Math.max(1,+this.currentTarget.textContent - 1)
   }
 }
